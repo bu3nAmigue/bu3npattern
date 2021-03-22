3d2eb27bbd97b2878970917eb07055b5bf4e3f23
sol <mssarratea@gmail.com>
2021-03-22 20:04:37 -0300

    dale

---
+++ helper.js
@@ -0,0 +1,22 @@
+var modal = document.getElementById("showInfo");
+var btn = document.getElementById("btn");
+var span = document.getElementsByClassName("close")[0];
+
+
+window.isPlay = false;
+modal.style.display="none";
+
+btn.onclick = function() {
+    modal.style.display = "block"
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+
+}
